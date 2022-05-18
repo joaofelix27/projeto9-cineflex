d750705fc2e57f81d096fa3e3b7cbf56e663fbc1
joaofelix27 <joaofelix27@gmail.com>
2022-05-17 23:08:42 -0300

    Tela 1 com axios.get OK

--- src/Componentes/ListaFIlmes.js
+++ src/Componentes/ListaFIlmes.js
@@ -1,24 +1,35 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function ListaFilmes () {
+export default function ListaFilmes() {
     const [filmes, setFilmes] = useState([]);
 
-	useEffect(() => {
-		const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+    useEffect(() => {
+        const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
 
-		requisicao.then(resposta => {
-			setItems(resposta.data.items);
-		});
-	}, []);
-    return(
+        requisicao.then(resposta => {
+            setFilmes(resposta.data);
+        });
+    }, []);
+    console.log(filmes);
+    return (
         <>
-        <header>
-        CINEFLEX
-        </header>
-        <section className="escolherFilme">
-            Selecione o filme
-        </section>
+            <header>
+                CINEFLEX
+            </header>
+            <section className="escolherFilme">
+                Selecione o filme
+            </section>
+            <section className='movieContainer'>
+                {
+                    filmes.map((value, index) =>
+                        <div className='movie'>
+                            <img className='moviePicture' src={value.posterURL} />
+                        </div>
+                    )
+                }
+            </section>
+
         </>
     )
 }
