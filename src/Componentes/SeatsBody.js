3bfb4c04dc139bd73412ea09b99132c4c3dc288d
joaofelix27 <joaofelix27@gmail.com>
2022-05-19 21:31:36 -0300

    Implementando clique nos assentos

--- src/Componentes/SeatsBody.js
+++ src/Componentes/SeatsBody.js
@@ -1,22 +1,27 @@
+import { useState, useEffect } from "react";
 export default function SeatsBody({ seats }) {
-
+    const [selecionado,setSelecionado]=useState(false)
     return (
         <div className="seatsContainer">
             {seats.seats.map((seat, index) =>
-                <div className={`${seat.isAvailable}`}>
+                <div onClick={() => setSelecionado("selecionado")} className={seat.isAvailable==true ? `true`:`${selecionado}`}>
                     {index + 1}
                 </div>
             )}
             <div className="legenda">
                 <div>
-                    Selecionado
+                    <div className="selecionado"></div>
+                    <h1>Selecionado</h1>
                 </div>
-                <div className="false">
-                    Disponível
+                <div>
+                    <div className="false1"></div>
+                    <h1>Disponível</h1>
                 </div>
-                <div className="true">
-                    Indisponível
+                <div>
+                    <div className="true1"></div>
+                    <h1>Indisponível</h1>
                 </div>
+
             </div>
 
         </div>
