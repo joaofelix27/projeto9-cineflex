dda4ae3d0410802779f86d6fc9250c20887f647b
joaofelix27 <joaofelix27@gmail.com>
2022-05-20 01:25:26 -0300

    Versão cliques no assentos OK!

--- src/Componentes/SeatsBody.js
+++ src/Componentes/SeatsBody.js
@@ -1,10 +1,37 @@
 import { useState, useEffect } from "react";
+
+function validar(seats1) {
+    const array = []
+    console.log(seats1)
+    for (let i = 0; i < seats1.length; i++) {
+        if (seats1[i].isAvailable == `selecionado1`) {
+            array.push(seats1[i].id)
+        }
+    }
+    return array
+}
+ function reservar (event) {
+     
+ }
 export default function SeatsBody({ seats }) {
-    const [selecionado,setSelecionado]=useState(false)
+    const [selecionado, setSelecionado] = useState(false)
+    const [seats1, setSeats1] = useState(seats.seats)
+    function zap(cardIndex) {
+        let seatsStatus = [...seats1]
+        if (seatsStatus[cardIndex].isAvailable == true) {
+            alert("Esse assento não está disponível")
+            return;
+        }
+        if (seatsStatus[cardIndex].isAvailable == `selecionado1`) {
+            seatsStatus[cardIndex].isAvailable = false
+        } else { seatsStatus[cardIndex].isAvailable = `selecionado1` }
+        setSeats1([...seatsStatus])
+    }
     return (
+
         <div className="seatsContainer">
-            {seats.seats.map((seat, index) =>
-                <div onClick={() => setSelecionado("selecionado")} className={seat.isAvailable==true ? `true`:`${selecionado}`}>
+            {seats1.map((seat, index) =>
+                <div onClick={() => zap(index)} className={seat.isAvailable == true ? `true` : seat.isAvailable == false ? `false` : `selecionado1`}>
                     {index + 1}
                 </div>
             )}
@@ -23,7 +50,11 @@ export default function SeatsBody({ seats }) {
                 </div>
 
             </div>
-
+            <form onSubmit={() =>validar(seats1)}>
+                <input type="email"  />
+                <input type="password"  />
+                <button type="submit">Reservar Assentos</button>
+            </form>
         </div>
     )
 
